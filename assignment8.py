637ba5b175533f78d22eeb016e3a49dde42087a3
Iryna <iryna.sherepot@gmail.com>
2019-11-19 19:04:35 -0800

    fixed the loggin issue

--- assignment8.py
+++ assignment8.py
@@ -10,37 +10,36 @@ FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
 # if somehow trace level was not created - log critical?
 # if logger is null - error, critical?
 
+DEBUG/TRACE messages can be logged in every function or method call in the application code and
+provide detailed information about the inputs and outputs from the method.
 '''
 
 TRACE_LEVEL = 5
+logFormat = '%(asctime)s - %(levelname)s - %(message)s'
+
+
 # creating custom trace level of logging
 def trace(self, message, *args, **kws):
     if self.isEnabledFor(TRACE_LEVEL):
         # Yes, logger takes its '*args' as 'args'.
         self._log(TRACE_LEVEL, message, args, **kws)
+
+
 logging.addLevelName(TRACE_LEVEL, "TRACE")
-#add trace level to a logger
+# add trace level to a logger
 logging.Logger.trace = trace
 
-logging.basicConfig(format='%(asctime)s %(message)s')
-#set the format, min level as tracing, file and filemode
-logging.basicConfig(format='%(asctime)s %(message)s % level', level=5, filename="consoleapp.log", filemode="write")
-
 # create console handler and set level to debug
-ch = logging.StreamHandler()
+ch = logging.FileHandler("countoccurences.log", "write")
 ch.setLevel(logging.DEBUG)
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# add formatter to ch
-ch.setFormatter(formatter)
-
+# set the format, min level as tracing, file and filemode
+logging.basicConfig(format=logFormat, level=5, filename="countoccurences.log", filemode="w")
 log = logging.getLogger("my-logger")
+
 # check if log object succesfully created
 if log is None:
     raise Exception("Logger was not created successfuly")
 # check and make sure if trace level was created
-
 if hasattr(log, 'trace'):
     log.debug("Custom trace attribute was created.")
 else:
@@ -50,7 +49,7 @@ else:
 
 
 def func(path, functionality, word):
-    if path is "":
+    if len(path) is 0:
         log.error("No file path provided, cannot have empty path")
     if functionality is "" or word is "":
         log.info("Functionality or word to search for are blank")
@@ -97,9 +96,8 @@ def func(path, functionality, word):
         log.debug("File closed")
 
     elif functionality == "write":
-        # When in "write" mode
-        # Average time to write a line in the file
-        # Average time to find the word "imperdiet" in the line
+
+        # writing to the same file in the append mode, adding new sentences to the end
         log.info("User chosed " + functionality)
         nOfInputWord = 0
         nOfInputSentences = -1
@@ -107,44 +105,43 @@ def func(path, functionality, word):
         totalTimeToFindWordInAllLines = 0
         totalTimeToWriteLines = 0
         try:
-            f = open(path, "write")
+            f = open(path, "a")
+            while sentence != "Exit":
+                log.debug("Entered loop menu asking user to enter sentences.")
+                sentence = raw_input(
+                    "\nPlease kindly enter the message you would like to write to a file. Use quatation marks. \nPlease press Exit to exit and count\n\n")
+                start = time.time()
+                f.write('\n' + sentence)
+                end = time.time()
+                timeToWriteALine = end - start
+                totalTimeToWriteLines += timeToWriteALine
+                nOfInputSentences += 1
+                nOfInputWord, nOfSentWordOccured, \
+                timeToFindWord = incrementCounts(sentence, nOfSentWordOccured,
+                                                 nOfInputWord, word)
+                totalTimeToFindWordInAllLines += timeToFindWord
+                log.debug("Counting number of sentences and number of time the word occurred completed")
+                if sentence == "Exit":
+                    log.info("User made choice Exit, exiting")
+                    log.debug("User terminated the menu loop")
+                    f.close()
+                    log.debug("File closed")
+                    break
+                elif input is ValueError:
+                    log.error("User entered incorrect choice, raised ValueError, continuing")
+                    continue
         except Exception as exception:
             log.error("Can not open file." + path, exc_info=True)
             print ("Can not open file: " + path)
         except IOError as e:
             log.error("File not open for writing.")
-        while sentence != "Exit":
-            log.debug("Entered loop menu asking user to enter sentences.")
-            sentence = raw_input(
-                "\nPlease kindly enter the message you would like to write to a file. Use quatation marks. \nPlease press Exit to exit and count\n\n")
-            start = time.time()
-            f.write(sentence)
-            end = time.time()
-            timeToWriteALine = end - start
-            totalTimeToWriteLines += timeToWriteALine
-            nOfInputSentences += 1
-            # start = time.time()
-            nOfInputWord, nOfSentWordOccured, \
-                            timeToFindWord = incrementCounts(sentence, nOfSentWordOccured,
-                                                                               nOfInputWord, word)
-            # end = time.time()
 
-            totalTimeToFindWordInAllLines += timeToFindWord
-            log.debug("Counting number of sentences and number of time the word occurred completed")
-            if sentence == "Exit":
-                log.info("User made choice Exit, exiting")
-                log.debug("User terminated the menu loop")
-                f.close()
-                log.debug("File closed")
-                break
-            elif input is ValueError:
-                log.error("User entered incorrect choice, raised ValueError, continuing")
-                continue
         # calculate everage time for all iterations
-        evarageTimeToWriteLines = totalTimeToWriteLines /nOfInputSentences
-        everageTime = totalTimeToFindWordInAllLines / nOfInputSentences
-        print "\nEverage time to find a word in the sentence " + "%.6f" % (everageTime) + "seconds."
-        print "Everage time to write a sentence to a file is " + "%.6f" %(evarageTimeToWriteLines) + "seconds."
+        evarageTimeToWriteLines = everage(nOfInputSentences, totalTimeToWriteLines)
+        everageTime = everage(nOfInputSentences, totalTimeToFindWordInAllLines)
+
+        printPerfomanceMetrics(evarageTimeToWriteLines, everageTime)
+
         log.info("User finished entering input, printing the results.")
         log.debug("Finished input loop, printing the results.")
         print("The user entered " + str(nOfInputSentences) + " sentences")
@@ -156,9 +153,21 @@ def func(path, functionality, word):
         raise ValueError("The functionality must be read or write only")
 
 
+# ----------------HELPER FUNCTIONS------------------------
+def everage(nOfInputSentences, totalTimeToWriteLines):
+    evarageTimeToWriteLines = totalTimeToWriteLines / nOfInputSentences
+    return evarageTimeToWriteLines
+
+
+def printPerfomanceMetrics(evarageTimeToWriteLines, everageTime):
+    print "\nEverage time to find a word in the sentence " + "%.6f" % (everageTime) + "seconds."
+    print "Everage time to write a sentence to a file is " + "%.6f" % (evarageTimeToWriteLines) + "seconds."
+
+
 def printWordCountMetrics(nOfInputWord, nOfSentWordOccured, word):
     print("the word 'imperdiet appear " + str(nOfInputWord) + " time(s).")
     print("Word " + word + " appeared in " + str(nOfSentWordOccured) + " sentence(s).")
+
 
 def incrementCounts(eachLine, nOfLines, timesPresent, word):
     start = time.time()
@@ -176,6 +185,5 @@ def incrementCounts(eachLine, nOfLines, timesPresent, word):
     return nOfLines, timesPresent, timeToFindWord
 
 
-
-# Testing
-func("/Users/student/Dropbox/assignment-8.txt", "write", "imperdiet")
+# ----------------TESTING------------------------
+func("/Users/student/Dropbox/assignment-8.txt", "read", "imperdiet")
